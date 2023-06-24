4600ee5464c6b79b575f7e0f41653a7baf6e7338
Jake Lohman <jakelohman7@gmail.com>
2023-06-24 09:33:06 -0500

    Delete index.html and script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("container");
-  const toggleCheckbox = document.getElementById("toggleCheckbox");
-
-  toggleCheckbox.addEventListener("change", function () {
-    const checked = toggleCheckbox.checked;
-    container.className = checked ? "myborder" : "";
-  });
-});
